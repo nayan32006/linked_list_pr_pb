b85ee4db842ebae050b35abb5f8c3d4378e4faa2
nayan32006 <nayang389@gmail.com>
2026-04-14 21:43:49 +0530

    DAY 2 OF LL : complet removeNthFromend

---
+++ removeNthFromend.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        while (fast->next != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
+};
+
+
+ListNode* createList(vector<int> arr) {
+    if (arr.empty()) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < arr.size(); i++) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << (head->next ? " -> " : "");
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Solution sol;
+    
+    vector<int> values = {1, 2, 3, 4, 5};
+    ListNode* head = createList(values);
+    int n = 2;
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = sol.removeNthFromEnd(head, n);
+
+    cout << "After removing " << n << "th from end: ";
+    printList(head);
+
+    return 0;
+}
