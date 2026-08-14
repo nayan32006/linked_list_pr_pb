ce84db892b5121f99abf4b47bbcb72c01794733d
nayan32006 <nayang389@gmail.com>
2026-08-14 22:38:08 +0530

    "feat: solve reorder list using fast-slow pointers and two-pointer merge in C++"

---
+++ reorder_list.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* secondHalf = reverseList(slow->next);
+        slow->next = nullptr;
+
+        ListNode* firstHalf = head;
+        while (secondHalf) {
+            ListNode* tmp1 = firstHalf->next;
+            ListNode* tmp2 = secondHalf->next;
+
+            firstHalf->next = secondHalf;
+            secondHalf->next = tmp1;
+
+            firstHalf = tmp1;
+            secondHalf = tmp2;
+        }
+    }
+};
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution sol;
+    sol.reorderList(head);
+
+    cout << "Reordered List: ";
+    printList(head);
+
+    return 0;
+}
